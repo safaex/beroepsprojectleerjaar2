36aa047a89543d306766c9eecee4b62c01a52f42
safaex <133200932+safaex@users.noreply.github.com>
2023-10-19 17:47:59 +0200

    Add files via upload

---
+++ SAFAE/klantservice.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title>Klantenservice - PHONIX</title>
+   <link rel="stylesheet" type="text/css" href="klant.css">
+</head>
+
+
+<body>
+
+
+<div class="banner">
+        <div class="navbar">
+            <img src="images/logo.png" class="logo">
+
+        <ul>
+            <li><a href="home.php">HOME</a></li>
+            <li><a href="overons.php">OVER ONS</a></li>
+            <li><a href="klantservice.php">KLANTENSERVICE</a></li>
+            <li><a href="onzeservice.php">ONZE SERVICE</a></li>
+            <li><a href="inloggen.php">INLOGGEN</a></li>
+        </ul>
+    </div>
+    
+    
+
+    <div class="service-container">
+        <h1>Klantenservice - PHONIX</h1>
+        <p>Bij PHONIX is klantenservice onze hoogste prioriteit. We begrijpen dat tevreden klanten de hoeksteen zijn van ons succes, en we zijn er trots op om u de best mogelijke ondersteuning te bieden.</p>
+        <div class="service-item">
+            <h2>Deskundige Ondersteuning</h2>
+            <p>Ons team van deskundigen staat klaar om uw vragen te beantwoorden en u te begeleiden bij het maken van de juiste keuzes. Of u nu technische assistentie nodig heeft bij het instellen van uw nieuwe telefoon, advies wilt over welk toestel het beste bij uw behoeften past, of gewoon meer informatie wilt over ons productaanbod, wij zijn hier om te helpen.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Snelle Respons</h2>
+            <p>Wij begrijpen het belang van een snelle respons. U kunt rekenen op een tijdige reactie op uw vragen en verzoeken. Ons doel is om u snel de informatie te bieden die u nodig heeft, of het nu gaat om productinformatie, orderstatus of technische ondersteuning.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Transparantie en Betrouwbaarheid</h2>
+            <p>Bij PHONIX geloven we in transparantie en betrouwbaarheid. Onze prijzen en voorwaarden zijn duidelijk en eerlijk. We zorgen ervoor dat u altijd op de hoogte bent van de status van uw bestelling en bieden u de flexibiliteit om wijzigingen aan te brengen wanneer dat nodig is.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Eenvoudige Retourprocedure</h2>
+            <p>Mocht u om welke reden dan ook niet tevreden zijn met uw aankoop, dan bieden we een eenvoudige retourprocedure. Ons doel is om ervoor te zorgen dat u tevreden bent met uw aankoop bij [Naam van de Webwinkel], en we zullen er alles aan doen om eventuele problemen op te lossen.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Toekomstige Ondersteuning</h2>
+            <p>Onze klantenservice stopt niet bij uw aankoop. We blijven beschikbaar voor eventuele vragen of problemen die zich in de toekomst kunnen voordoen. Of het nu gaat om technische ondersteuning, software-updates of aanvullende accessoires, we blijven uw partner in uw mobiele reis.</p>
+        </div>
+        
+        <a href="contact.php" class="contact-button">Neem contact met ons op</a>
+    </div>
+</body>
+
+</html>
---
+++ SAFAE/onzeservice.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+    
+<head>
+   <title>Over Onze Service</title>
+   <link rel="stylesheet" type="text/css" href="service.css">
+</head>
+<body>
+<div class="banner">
+        <div class="navbar">
+            <img src="images/logo.png" class="logo">
+
+        <ul>
+            <li><a href="home.php">HOME</a></li>
+            <li><a href="overons.php">OVER ONS</a></li>
+            <li><a href="klantservice.php">KLANTENSERVICE</a></li>
+            <li><a href="onzeservice.php">ONZE SERVICE</a></li>
+            <li><a href="inloggen.php">INLOGGEN</a></li>
+        </ul>
+    </div>
+
+
+    <div class="service-container">
+        <div class="service-item">
+            <h2>Deskundig Advies</h2>
+            <p>Ons team van deskundigen staat altijd klaar om u te helpen bij het maken van de juiste keuzes. Of u nu technische vragen heeft over een specifiek toestel, advies nodig heeft over welke telefoon het beste bij uw behoeften past, of gewoon wat hulp nodig heeft bij het navigeren door ons aanbod, we zijn er om u te begeleiden.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Snelle en Veilige Levering</h2>
+            <p>We begrijpen dat u uw nieuwe telefoon snel in handen wilt hebben. Daarom werken we samen met vertrouwde bezorgpartners om ervoor te zorgen dat uw bestelling snel en veilig bij u wordt afgeleverd. We houden u op de hoogte van de status van uw bestelling en zorgen ervoor dat deze in perfecte staat bij u aankomt.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Uitstekende Klantenservice</h2>
+            <p>Uw tevredenheid staat bij ons op de eerste plaats. Als u vragen heeft, problemen ondervindt of gewoon meer informatie nodig heeft, aarzel dan niet om contact op te nemen met onze klantenservice. We zijn hier om naar u te luisteren en u de ondersteuning te bieden die u verdient.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Eenvoudig Retourbeleid</h2>
+            <p>Bij [Naam van de Webwinkel] begrijpen we dat dingen soms anders uitpakken dan verwacht. Daarom bieden we een eenvoudig en transparant retourbeleid. Als u om welke reden dan ook niet tevreden bent met uw aankoop, helpen we u graag bij het retourneren van het product.</p>
+        </div>
+
+        <div class="service-item">
+            <h2>Voortdurende Ondersteuning</h2>
+            <p>Onze service eindigt niet bij uw aankoop. We bieden voortdurende ondersteuning om ervoor te zorgen dat uw telefoon optimaal blijft presteren. Of het nu gaat om technische problemen, software-updates of accessoires, we staan klaar om u te helpen.</p>
+        </div>
+    </div>
+</body>
+
+</html>
